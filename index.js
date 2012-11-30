5f929e323a6fc4a051a08a2b8f8c6e9b821db2f3
Matthew Hudson <matt@matthewghudson.com>
2012-11-29 20:59:04 -0500

    Transition from expressjs to node-webpipe.

--- index.js
+++ index.js
@@ -1,82 +1,32 @@
-var _ = require('underscore'),
-	marked = require('marked'),
-	express = require('express'),
-	app = express();
+var marked = require('marked');
+var webpipes = require('node-webpipe');
 
-app.use(express.bodyParser());
+// Option 1: Chaining API
+new webpipes.Block()
+  .name("Parse Markdown")
+  .description("Converts Markdown to HTML.")
+  .input("markdown", "string", "Markdown-formatted content for transformation.")
+  .output("html", "string", "HTML-converted data.")
+  .handle(function(inputs) {
+    return { html: marked(inputs.markdown) };
+  })
+  .listen();
 
-// Simple logger
-app.use(function (request, response, next) {
-	console.log('%s %s', request.method, request.url);
-	next();
-});
-
-// Handle uncaughtException
-process.on('uncaughtException', function (error) {
-	exit('Error: ' + error.message);
-});
-
-var exit = function (message) {
-	if (message) {
-		console.log(message);
-	}
-	console.log('Exiting...');
-	process.exit(1);
-};
-
-app.options('/', function (request, response) {
-
-	// CORS support
-	response.set('Access-Control-Allow-Origin', '*');
-	response.set('Access-Control-Allow-Methods', 'OPTIONS,POST');
-	response.set('Access-Control-Allow-Headers', 'Content-Type');
-
-	// The block definition
-	response.send({
-		name: "Parse Markdown",
-		description: "Converts Markdown to HTML.",
-		inputs: [{
-			name: "markdown",
-			type: "string",
-			description: "Markdown-formatted content for transformation."
-		}],
-		outputs: [{
-			name: "html",
-			type: "string",
-			description: "HTML-converted data."
-		}]
-	});
-});
-
-app.post('/', function (request, response) {
-
-	if (!_.has(request.body, 'inputs') && isArray(request.body.inputs)) {
-		exit('WebPipe "input" is missing or formatted incorrectly.');
-	}
-
-	var inputs = request.body.inputs[0];
-	var outputs = {
-		outputs: []
-	};
-	
-	// Verify POST keys exist
-	if (!_.has(inputs, 'markdown')) {
-		exit('Input is missing required "markdown" content.');
-	}
-	
-	var html = marked(inputs.markdown);
-	if (!html) {
-		console.log('Failed to convert markdown.');
-		response.send(500);
-	} else {
-		outputs.outputs.push({
-			html: html
-		});
-		response.json(outputs);
-	}
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port, function () {
-	console.log('Listening on ' + port);
-});
+// Option 2: Pass in block definition
+// var block = new webpipes.Block({
+//   "name": "Parse Markdown",
+//   "description": "Converts Markdown to HTML.",
+//   "inputs": [{
+//       "name": "markdown",
+//       "type": "string",
+//       "description": "Markdown-formatted content for transformation."
+//   }],
+//   "outputs": [{
+//       "name": "html",
+//       "type": "string",
+//       "description": "HTML-converted data."
+//   }]
+//  }, function(inputs) {
+//   return { html: marked(inputs.markdown) };
+// });
+// http.createServer(block.nodeHandler()).listen(process.env.PORT || 3000);
